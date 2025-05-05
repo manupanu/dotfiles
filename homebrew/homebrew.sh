06f297b794d511bde6afdcd028a894dddb0f84f7
Manuel Anrig <me@manuelanrig.ch>
2025-05-05 12:03:50 +0200

    Add Homebrew installation script and Brewfile for package management

---
+++ homebrew/homebrew.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/cpojer/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+brew update
+brew bundle
