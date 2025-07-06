1ec0505902bf7dcd01a07f5058007b3c1fbc95f9
Manuel Anrig <me@manuelanrig.ch>
2025-07-06 14:43:20 +0200

    Add additional API keys to secrets.zsh
    Expanded secrets.zsh to include environment variables for Atalassian, Gemini, and OpenAI API keys and user. Also updated comments for clarity and changed the JIRA API key reference.

--- zsh/secrets.zsh
+++ zsh/secrets.zsh
@@ -1,1 +1,14 @@
-export JIRA_API_KEY="op://Private/JIRA API Dashboard/credential"
+# This file is sourced by zsh to set environment variables for secrets.
+# It is not meant to be executed directly.
+# Ensure this file is sourced in your .zshrc or equivalent configuration file.
+
+# Atalassian API Token
+export JIRA_API_KEY="op://Private/Atalassian API/credential"
+export JIRA_API_USER="op://Private/Atalassian API/username"
+
+# Gemini API Key
+export GEMINI_API_KEY="op://Private/Gemini API/credential"
+
+# OpenAI API Key
+export OPENAI_API_KEY="op://Private/openai API/credential"
+
