d2a18bac3dee999fedb6bac9afe7aa10409b1b29
Manuel Anrig <me@manuelanrig.ch>
2025-08-01 17:53:53 +0200

    Refactor wezterm configuration: optimize appearance settings and remove unused gradient options

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,61 +12,25 @@ config.initial_rows = 28
 
 -- Appearance settings:
 config.font_size = 14
+config.initial_cols = 150
+config.initial_rows = 40
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.color_scheme = 'Catppuccin Mocha'
-config.window_background_opacity = 1
-config.macos_window_background_blur = 20 -- For macOS, this will apply a blur effect to the window background.
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+config.window_decorations = 'RESIZE'
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+config.default_cursor_style = 'BlinkingBlock'
+config.window_padding = {
+		left = 10,
+		right = 10,
+		top = 10,
+		bottom = 10,
+	}
 
-config.window_background_gradient = {
-  -- Can be "Vertical" or "Horizontal".  Specifies the direction
-  -- in which the color gradient varies.  The default is "Horizontal",
-  -- with the gradient going from left-to-right.
-  -- Linear and Radial gradients are also supported; see the other
-  -- examples below
-  orientation = 'Vertical',
 
-  -- Specifies the set of colors that are interpolated in the gradient.
-  -- Accepts CSS style color specs, from named colors, through rgb
-  -- strings and more
-  colors = {
-    '#0f0c29',
-    '#302b63',
-    '#24243e',
-  },
-
-  -- Instead of specifying `colors`, you can use one of a number of
-  -- predefined, preset gradients.
-  -- A list of presets is shown in a section below.
-  -- preset = "Warm",
-
-  -- Specifies the interpolation style to be used.
-  -- "Linear", "Basis" and "CatmullRom" as supported.
-  -- The default is "Linear".
-  interpolation = 'Linear',
-
-  -- How the colors are blended in the gradient.
-  -- "Rgb", "LinearRgb", "Hsv" and "Oklab" are supported.
-  -- The default is "Rgb".
-  blend = 'Rgb',
-
-  -- To avoid vertical color banding for horizontal gradients, the
-  -- gradient position is randomly shifted by up to the `noise` value
-  -- for each pixel.
-  -- Smaller values, or 0, will make bands more prominent.
-  -- The default value is 64 which gives decent looking results
-  -- on a retina macbook pro display.
-  -- noise = 64,
-
-  -- By default, the gradient smoothly transitions between the colors.
-  -- You can adjust the sharpness by specifying the segment_size and
-  -- segment_smoothness parameters.
-  -- segment_size configures how many segments are present.
-  -- segment_smoothness is how hard the edge is; 0.0 is a hard edge,
-  -- 1.0 is a soft edge.
-
-  -- segment_size = 11,
-  -- segment_smoothness = 0.0,
-}
 
 -- Finally, return the configuration to wezterm:
 return config
